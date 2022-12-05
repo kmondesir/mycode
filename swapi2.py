4b1d6e586701b7edee97d737c2ebff9bdb9d55f8
kmondesir <kino.mondesir@gmail.com>
2022-12-05 21:07:46 +0000

    First API request

---
+++ swapi2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Alta3 Research
+   Star Wars API HTTP response parsing"""
+
+# pprint makes dictionaries a lot more human readable
+from pprint import pprint
+
+# requests is used to send HTTP requests (get it?)
+import requests
+
+#URL = "https://swapi.dev/luke/force"      # Comment out this line
+URL= "https://swapi.dev/api/people/4/"     # Uncomment this line
+URL1 = "https://swapi.dev/api/films/1/"
+URL2 = "https://swapi.dev/api/starships/13/"
+def main():
+    """sending GET request, checking response"""
+
+    # SWAPI response is stored in "resp" object
+    resp = requests.get(URL)
+    resp2 = requests.get(URL1)
+    resp3 = requests.get(URL2)
+    # check to see if the status is anything other than what we want, a 200 OK
+    if resp.status_code == 200:
+        # convert the JSON content of the response into a python dictionary
+        vader= resp.json()
+        pprint(vader)
+        pprint(resp2.json())
+        pprint(resp3.json())
+        print(f"{vader['name']} was born in the year {vader['birth_year']}")
+    else:
+        print("That is not a valid URL.")
+
+if __name__ == "__main__":
+    main()
+
